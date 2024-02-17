1abc5e0764684a0e17ea44893e208b73627eb8ec
Geon.C <chlrjs1324@gmail.com>
2024-02-17 23:05:36 +0900

    기본 응답 및 예외 양식 구성
    Response : 기본 응답을 구성하는 클래스, 성공 & 실패에 따른 결과 분류
    
    CustomException : RuntimeException을 상속 받은 커스텀 예외 클래스, ResponseStatus의 code에 따른 예외 응답 처리
    
    GlobalExceptionHandler : 전역 예외처리를 담당하는 클래스
    
    ResponseStatus : 응답 결과에 따른 상태 정보를 담당하는 클래스

--- src/main/java/com/core/toy3/common/response/Response.java
+++ src/main/java/com/core/toy3/common/response/Response.java
@@ -1,4 +1,44 @@
 package com.core.toy3.common.response;
 
-public class Response {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.core.toy3.common.response.ResponseStatus.*;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "statusCode", "message", "data"})
+public class Response<T> {
+
+    private Boolean isSuccess;
+    private Integer statusCode;
+    private String message;
+    private ResponseStatus status;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    // 성공시 응답 모델
+    public static <T> Response<T> response(T data) {
+        return new Response<>(
+                SUCCESS.isSuccess(),
+                SUCCESS.getStatusCode(),
+                SUCCESS.getMessage(),
+                SUCCESS,
+                data
+        );
+    }
+
+    // 실패시 응답
+    public static <T> Response<T> response(ResponseStatus status) {
+        return new Response<>(
+                status.isSuccess(),
+                status.getStatusCode(),
+                status.getMessage(),
+                status,
+                null
+        );
+    }
 }
