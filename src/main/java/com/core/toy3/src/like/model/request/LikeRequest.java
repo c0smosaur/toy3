e71416e556121692cb4ae67149cc83d40a98b0fb
Jiyeong Kim <150122029+jisoon418@users.noreply.github.com>
2024-02-20 13:55:37 +0900

    #3어노테이션 수정

--- src/main/java/com/core/toy3/src/like/model/request/LikeRequest.java
+++ src/main/java/com/core/toy3/src/like/model/request/LikeRequest.java
@@ -1,22 +1,15 @@
 package com.core.toy3.src.like.model.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LikeRequest {
     private Long memberId;
     private Long travelId;
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public Long getTravelId() {
-        return travelId;
-    }
-
-    public void setTravelId(Long travelId) {
-        this.travelId = travelId;
-    }
-
 }
--- src/main/java/com/core/toy3/src/like/model/response/LikeResponse.java
+++ src/main/java/com/core/toy3/src/like/model/response/LikeResponse.java
@@ -1,9 +1,13 @@
 package com.core.toy3.src.like.model.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@Builder
+@ToString
 @AllArgsConstructor
 public class LikeResponse {
     private Long likeId;
